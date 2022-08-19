bccf15e32b15fb1da540d46c28b00da899a2a729
sin90is1 <sinaghavamy.sg79@gmail.com>
2022-08-19 19:10:22 +0430

    healing icon ability & 2 c++ class to get ability info in the UI
    Create the healing icon ability material and widget with some functionality and Implemented 2 cpp class to get ability info like cost and cooldown & .. in the UI

---
+++ Source/VampireALusth/Public/GameplayAbilityBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GameplayAbilityBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VAMPIREALUSTH_API UGameplayAbilityBase : public UGameplayAbility
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AbilityBase")
+		UMaterialInstance* UIMaterial;
+};
