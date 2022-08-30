be0f701aa762cdd2f1997f1b6c9f243edb676b9f
sin90is1 <sinaghavamy.sg79@gmail.com>
2022-08-31 00:28:55 +0430

    Start the next Ability , The FireBlast
    the around enemies on a specific radius will pulled to the character

---
+++ Source/VampireALusth/Private/GATargetActorAround.cpp
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GATargetActorAround.h"
+#include "Abilities/GameplayAbility.h"
+#include "GameFramework/Pawn.h"
+#include "GameFramework/PlayerController.h"
+
+void AGATargetActorAround::StartTargeting(UGameplayAbility* Ability)
+{
+	OwningAbility = Ability;
+	MasterPC = Cast<APlayerController>(Ability->GetOwningActorFromActorInfo()->GetInstigatorController());
+}
+
+void AGATargetActorAround::ConfirmTargetingAndContinue()
+{
+	APawn* OwningPawn = MasterPC->GetPawn();
+	if (!OwningPawn)
+	{
+		TargetDataReadyDelegate.Broadcast(FGameplayAbilityTargetDataHandle());
+		return;
+	}
+	FVector ViewLocation = OwningPawn->GetActorLocation();
+
+	TArray<FOverlapResult> Overlaps;
+	TArray<TWeakObjectPtr<AActor>> OverlapedActors;
+	bool TraceComplex = false;
+
+	FCollisionQueryParams CollisionQueryParams;
+	CollisionQueryParams.bTraceComplex = TraceComplex;
+	CollisionQueryParams.bReturnPhysicalMaterial = false;
+	APawn* Masterpawn = MasterPC->GetPawn();
+	if (Masterpawn)
+	{
+		CollisionQueryParams.AddIgnoredActor(Masterpawn->GetUniqueID());
+	}
+
+	bool TryOverlap = GetWorld()->OverlapMultiByObjectType(
+		Overlaps,
+		ViewLocation,
+		FQuat::Identity,
+		FCollisionObjectQueryParams(ECC_Pawn),
+		FCollisionShape::MakeSphere(Radius),
+		CollisionQueryParams);
+	if (TryOverlap)
+	{
+		for (int32 i = 0; i < Overlaps.Num(); ++i)
+		{
+			APawn* PawnOverlaped = Cast<APawn>(Overlaps[i].GetActor());
+			if (PawnOverlaped && !OverlapedActors.Contains(PawnOverlaped))
+			{
+				OverlapedActors.Add(PawnOverlaped);
+			}
+		}
+	}
+
+	if (OverlapedActors.Num() > 0)
+	{
+		FGameplayAbilityTargetDataHandle TargetData = StartLocation.MakeTargetDataHandleFromActors(OverlapedActors);
+		TargetDataReadyDelegate.Broadcast(TargetData);
+	}
+	else
+	{
+		TargetDataReadyDelegate.Broadcast(FGameplayAbilityTargetDataHandle());
+	}
+}
